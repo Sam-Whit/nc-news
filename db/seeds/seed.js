408ad4816286b07f0431ce3ca2a789acb6369ca3
samwhit <sam_whitworth@hotmail.com>
2021-11-01 14:39:57 +0000

    inserting script phase start

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -5,25 +5,53 @@ const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
   return db
-    .query("DROP TABLE IF EXISTS topics;")
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users;`);
-    })
+    .query("DROP TABLE IF EXISTS comments;")
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS articles;`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS comments;`);
+      return db.query(`DROP TABLE IF EXISTS users;`);
     })
     .then(() => {
-      return db
-        .query(
-          `CREATE TABLE topics (
-      slug PRIMARY KEY,
+      return db.query(`DROP TABLE IF EXISTS topics;`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE topics (
+      slug VARCHAR(200) PRIMARY KEY,
       description VARCHAR(200)
-    );`
-        )
-        .then();
+    );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+      username VARCHAR(20) PRIMARY KEY,
+      avatar_url VARCHAR,
+      name VARCHAR(200)
+    );`); //ask about NVARCHAR for url
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title VARCHAR,
+        body VARCHAR,
+        votes INT DEFAULT 0,
+        topic TEXT REFERENCES topics(slug),
+        author TEXT REFERENCES users(username),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY,
+        author TEXT REFERENCES users(username),
+        article_id INT REFERENCES articles(article_id),
+        votes INT DEFAULT 0,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        body VARCHAR
+      )`);
+    })
+    .then(() => {
+      console.log("inserting");
+      return db.query();
     });
   // 2. insert data
 };
