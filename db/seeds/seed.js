2cdc348cde34c0cd4ca9a28be80e3ddd0635c2fa
samwhit <sam_whitworth@hotmail.com>
2021-11-01 12:15:11 +0000

    seeding SQL progress

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,9 +1,30 @@
-const db = require("../connection/index");
+const db = require("../connection.js");
 const format = require("pg-format");
 
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
+  return db
+    .query("DROP TABLE IF EXISTS topics;")
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS comments;`);
+    })
+    .then(() => {
+      return db
+        .query(
+          `CREATE TABLE topics (
+      slug PRIMARY KEY,
+      description VARCHAR(200)
+    );`
+        )
+        .then();
+    });
   // 2. insert data
 };
 
