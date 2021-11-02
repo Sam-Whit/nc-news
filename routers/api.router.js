a20ac857e9547372adf5ce98b9705cd2ffa83ce2
samwhit <sam_whitworth@hotmail.com>
2021-11-02 11:48:30 +0000

    end points set-up

---
+++ routers/api.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const topicsRouter = require("./topics.router");
+
+const apiRouter = express.Router();
+
+apiRouter.use("/topics", topicsRouter);
+
+module.exports = apiRouter;
---
+++ routers/topics.router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const topicsRouter = express.Router();
+
+//app.js for topics paths
+
+module.exports = topicsRouter;
