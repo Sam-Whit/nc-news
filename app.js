46485e6e323215e75df6c2089d20184b87b722ec
samwhit <sam_whitworth@hotmail.com>
2021-11-02 22:04:34 +0000

    404 custom error handling article by ID

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.all("/*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request, invalid input" });
   }
