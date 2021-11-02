a7b1813332049115464bdb1676b93bac1562628f
samwhit <sam_whitworth@hotmail.com>
2021-11-02 23:06:52 +0000

    article patch controller

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,4 @@
-const { fetchArticle } = require("../models/articles.model.js");
+const { fetchArticle, patchVotes } = require("../models/articles.model.js");
 
 exports.getArticle = (req, res, next) => {
   const { article_id } = req.params;
@@ -7,5 +7,14 @@ exports.getArticle = (req, res, next) => {
     .then((article) => {
       res.status(200).send({ article });
     })
+    .catch(next);
+};
+
+exports.patchArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+
+  patchVotes(article_id, inc_votes)
+    .then((data) => res.status(201).send({ article: data }))
     .catch(next);
 };
